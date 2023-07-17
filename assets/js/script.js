662aaafb4338ece577398dd09a34f74502b3fb1f
JoFel7 <josuefeliciano7@gmail.com>
2023-07-17 10:57:39 -0400

    Added fetch for drink data to js and neccesary id tags to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,55 @@
-async function logDrink() {
-    const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita");
-    const drinks = await response.json();
+// async function logDrink() {
+//     const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita");
+//     const drinks = await response.json();
 
-    console.log(drinks)
+//     console.log(drinks)
 
-};
+// };
 
-logDrink();
+// logDrink();
 
-var cocktailEl = document.getElementById("Cocktail")
-var liquorEl = document .getElementById("Liquor")
+var cocktailEl = document.querySelector("#Cocktail");
+var liquorEl = document .querySelector("#Liquor");
+var cocktailInput = document.querySelector('#cocktailTxt');
+var drinkContainerEl = document.querySelector('#drink-container');
 
 cocktailEl.addEventListener("click", () => {
-    location.href = 'cocktail.html'
+    location.href = 'cocktail.html';
 })
 
 liquorEl.addEventListener("click", () => {
-    location.href = 'liquor.html'
+    location.href = 'liquor.html';
 })
 
-var inputCoctail = ""
-var drinkUrl =  "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    var cocktail = cocktailInput.value.trim();
+    if (cocktail) {
+        getUserRepos(cocktail);
+
+        drinkContainerEl.textContent = '';
+        cocktailInput.value = '';
+    } else {
+        alert('Please enter a Cocktail');
+    }
+};
+
+var getDrink = function (drink) {
+    var drinkUrl =  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + drink ;
+
+    fetch(drinkUrl).then(function (response) {
+        if (response.ok) {
+            console.log(response)
+            response.json().then(function (data) {
+                console.log(data);
+                displayDrinks(data, drink);
+            });
+        } else {
+            alert('Error: ' + response.statusText);
+        }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to CocktailDB');
+        });
+    };
