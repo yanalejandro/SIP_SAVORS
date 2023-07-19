6df241e6a3e4436b8d6f2fa65b566793dbbd998a
JoFel7 <josuefeliciano7@gmail.com>
2023-07-19 09:01:47 -0400

    Added changes with tailwind and js

--- assets/js/drink.js
+++ assets/js/drink.js
@@ -1,3 +1,6 @@
+const clientId = '74cac96df16a41c88732acf98714dbe5';
+const clientSecret = '85ad0afcb88f4cd4baecdcc4997483a8';
+
 //get the drink from local storage
 selectedDrink = localStorage.getItem('selected-drink');
 
@@ -57,21 +60,66 @@ function displayDrinks(recipe) {
 };
 
 async function _getToken() {
-    const url = '';
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '886b8f4589msh4b5135157d7d5bep1413bajsnc84e14b3155d',
-            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        const result = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded', 
+                'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
+            },
+            body: 'grant_type=client_credentials'
+        });
+
+        const data = await result.json();
+        console.log(data);
+        return data.access_token;
+    }
+
+_getToken();
+
+function getSong (song) {
+    var songUrl = 'https://api.spotify.com/v1/tracks/' + song ;
+    fetch(songUrl).then(function (response) {
+        if (response.ok) {
+            console.log(response)
+            response.json().then(function (data) {
+                console.log(data);
+                // displaySong(data);
+            });
         }
-    };
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-    };
+    });
 };
 
-logSong();
+// function displaySong(tunes) {
+//     var track = tunes.drinks[0];
+//     var trackDiv = document.getElementById("drink-search-term");
+//     // cocktail name
+//     trackDiv.innerHTML = '';
+//     var trackName = cocktail.strDrink;
+//     var heading = document.createElement("h1");
+//     heading.innerHTML = trackName;
+//     // cocktailDiv.appendChild("");
+//     trackDiv.appendChild(heading);
+//     // cocktail image
+//     var trackImg = document.createElement("img");
+//     trackImg.src = track.strDrinkThumb;
+//     trackDiv.appendChild(cocktailImg);
+//     // cocktail ingredients
+//     var cocktailIngredients = document.createElement("ul");
+//     cocktailDiv.appendChild(cocktailIngredients);
+//     var getIngredients = Object.keys(cocktail)
+//     .filter(function (ingredient) {
+//         return ingredient.indexOf("strIngredient") == 0;
+//     })
+//     .reduce(function (ingredients, ingredient) {
+//         if (cocktail[ingredient] != null) {
+//             ingredients[ingredient] = cocktail[ingredient];
+//         }
+//         return ingredients;
+//     }, {});
+//     for (var key in getIngredients) {
+//         var value = getIngredients[key];
+//         listItem = document.createElement("li");
+//         listItem.innerHTML = value;
+//         cocktailIngredients.appendChild(listItem);
+//     }
+// };
